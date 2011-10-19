06044b892c30c20ca18048506436b59d5a72857f
Zach Fogg <zach@zachfogg.com>
2011-10-18 19:55:21 -0500

    Working with my couchdb :D

--- simpleblog.coffee
+++ simpleblog.coffee
@@ -1,12 +1,27 @@
 bogart = require "bogart"
-viewEngine = bogart.viewEngine 'mustache'
+couchdb = require "couchdb"
+dbConfig =
+    user: "zach"
+    pass: "5984"
 
 app = bogart.router (get, post, update, destroy) ->
+    client     = couchdb.createClient 8080, "127.0.0.1", dbConfig
+    db         = client.db "blog"
+    viewEngine = bogart.viewEngine "mustache"
+
     get '/', () ->
-        bogart.html "Hello, world!"
+        bogart.html "Hello, node!"
 
     get "/posts/new", (request) ->
         title = locals: title: 'New Post'
         viewEngine.respond "new-post.html", title
 
+    post "/posts", (request) ->
+        pos = request.params
+        post.type = "post"
+
+        db.saveDoc.then (response) ->
+            bogart.redirect "/posts"
+
+app = bogart.middleware.ParseForm app
 bogart.start app
--- simpleblog.js
+++ simpleblog.js
@@ -1,20 +1,35 @@
-(function() {
-  var app, bogart, viewEngine;
-  bogart = require("bogart");
-  viewEngine = bogart.viewEngine('mustache');
-  app = bogart.router(function(get, post, update, destroy) {
-    get('/', function() {
-      return bogart.html("Hello, world!");
-    });
-    return get("/posts/new", function(request) {
-      var title;
-      title = {
-        locals: {
-          title: 'New Post'
-        }
-      };
-      return viewEngine.respond("new-post.html", title);
+var app, bogart, couchdb, dbConfig;
+bogart = require("bogart");
+couchdb = require("couchdb");
+dbConfig = {
+  user: "zach",
+  pass: "5984"
+};
+app = bogart.router(function(get, post, update, destroy) {
+  var client, db, viewEngine;
+  client = couchdb.createClient(8080, "127.0.0.1", dbConfig);
+  db = client.db("blog");
+  viewEngine = bogart.viewEngine("mustache");
+  get('/', function() {
+    return bogart.html("Hello, node!");
+  });
+  get("/posts/new", function(request) {
+    var title;
+    title = {
+      locals: {
+        title: 'New Post'
+      }
+    };
+    return viewEngine.respond("new-post.html", title);
+  });
+  return post("/posts", function(request) {
+    var pos;
+    pos = request.params;
+    post.type = "post";
+    return db.saveDoc.then(function(response) {
+      return bogart.redirect("/posts");
     });
   });
-  bogart.start(app);
-}).call(this);
+});
+app = bogart.middleware.ParseForm(app);
+bogart.start(app);
